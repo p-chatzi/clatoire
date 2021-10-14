83c6adcb616768a173d513337d170055613f6d39
p-chatzi <45150194+p-chatzi@users.noreply.github.com>
2021-10-14 10:36:04 +0200

    TP 3 Completed

---
+++ Clatoire/TP3/tp3-cin1.js
@@ -0,0 +1,7 @@
+function submit_info() {
+    alert("Infos envoyer! (C'est fake, juste une alert qui pop sur le clique ;) )");
+}
+
+function submit_cv() {
+    alert("CV envoyer! (C'est fake, juste une alert qui pop sur le clique ;) )");
+}
